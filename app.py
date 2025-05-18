a511d0a1f5ad09b6291da4c66cd68a4985f82e74
ServizoDobre <contact@servizodobre.com>
2025-05-17 22:37:08 -0400

    index render issue fix

--- app.py
+++ app.py
@@ -1,12 +1,12 @@
 from flask import Flask, render_template
 
-app = Flask(__name__, template_folder='templates')  # Use 'template' instead of 'templates'
+app = Flask(__name__)
 
 @app.route('/')
 def index():
-    # Render index.html from the templates directory
+    print("Rendering index.html")  # Debugging log
     return render_template('index.html')
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0')
+    app.run(debug=True)
 
